cf696abd143abc3f9f2d890959b17d9bd7516fff
rlaisqls <rlaisqls@gmail.com>
2023-07-22 16:52:02 +0900

    update :: remove yq function

--- check_config.sh
+++ check_config.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-function check_required_key() {
-  required_keys=("config.name" "config.prefix")
-  # $1 = first arg = file_path
-  for required_key in "${required_keys[@]}"; do
-    if ! yq eval ".\"${required_key}\"" "$1" &> /dev/null; then
-      echo "Error: The specified key \"$required_key\" does not exist"
-      exit 1
-    fi
-  done
-}
+github_raw_url="https://raw.githubusercontent.com/$GIT_REPO/${TARGET_BRANCH#refs/heads/}/$CONFIG_FILE_PATH"
+file_content=$(curl -sS $github_raw_url)
 
-github_raw_url="https://raw.githubusercontent.com/$GIT_REPO/$TARGET_BANCH/$CONFIG_FILE_PATH"
-file_path="/tmp/$(uuidgen)/config.yaml"
-curl -sS $github_raw_url -o $file_path
+required_keys=("config.name" "config.prefix")
 
-check_required_key $file_path
-
-rm $file_path
+for required_key in "${required_keys[@]}"; do
+  if ! echo "$file_content" | yq eval ".${required_key}" - > /dev/null; then
+    echo "Error: The specified key \"$required_key\" does not exist"
+    exit 1
+  fi
+done
