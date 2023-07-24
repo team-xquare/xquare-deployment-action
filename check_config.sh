d0622f14607c925d49ef33b61e1c6d5f9a23422b
rlaisqls <rlaisqls@gmail.com>
2023-07-24 13:21:57 +0900

    update :: domain 관련 설정 추가
    update :: yq 리팩토링
    
    
    refactor :: 리팩토링
    
    
    feat :: create domain

--- check_config.sh
+++ check_config.sh
@@ -3,11 +3,30 @@
 github_raw_url="https://raw.githubusercontent.com/$GIT_REPO/${TARGET_BRANCH#refs/heads/}/$CONFIG_FILE_PATH"
 file_content=$(curl -sS $github_raw_url)
 
-required_keys=("config.name")
+function readValue {
+  echo $(echo "$file_content" | yq eval $1 - > /dev/null)
+}
+
+# config 파일에 필요한 키가 존재하는지 여부 확인
+required_keys=(".config.name" ".config.service_type")
 
 for required_key in "${required_keys[@]}"; do
-  if ! echo "$file_content" | yq eval ".${required_key}" - > /dev/null; then
+  if ! $(readValue "${required_key}"); then
     echo "Error: The specified key \"$required_key\" does not exist"
     exit 1
   fi
 done
+
+# service_domain 값이 존재하면서 .xquare.app으로 끝나지 않는 경우 에러
+domain_key=".config.domain"
+domain_value=$(readValue "${domain_key}")
+
+if [[ ! $domain_value = "" && ! "$domain_value" =~ \.xquare\.app$ ]]; then
+  echo "Error: The domain ($domain_value) does not end with '.xquare.app'."
+  exit 1
+fi
+
+echo "name=$(readValue ".config.name")" >> $GITHUB_OUTPUT
+echo "prefix=$(readValue ".config.prefix")" >> $GITHUB_OUTPUT
+echo "domain=$(readValue ".config.domain")" >> $GITHUB_OUTPUT
+echo "type=$(readValue ".config.service_type")" >> $GITHUB_OUTPUT
